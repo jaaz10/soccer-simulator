7e1c7f733d71502cb171277dfe8b063b949461d7
Jaaziah Espinoza <jaazespinoza@gmail.com>
2026-04-22 12:03:32 -0500

    Testing Random Lib and the Player class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,25 @@
 package main;
 
+import java.util.Random;
+
 public class Main {
-	public static void main (String[] args) {
-		System.out.println("hello world");
-	}
+    public static void main(String[] args) {
+        System.out.println("=== Testing Random for Soccer Simulator ===\n");
+        
+        Random random = new Random();
+        
+        // Test random number generation
+        System.out.println("Rolling dice 5 times (0-100):");
+        for (int i = 1; i <= 5; i++) {
+            int roll = random.nextInt(100);
+            System.out.println("Roll " + i + ": " + roll);
+        }
+        
+        // Test coin flip
+        System.out.println("\nCoin flip: " + (random.nextBoolean() ? "Team A attacks" : "Team B attacks"));
+        
+        // Test player
+        Player player = new Player("Messi", 85);
+        System.out.println("\nCreated player: " + player.getName() + " (Skill: " + player.getSkillLevel() + ")");
+    }
 }
